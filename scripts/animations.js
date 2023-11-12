26c707547522a5e2f902329d55dbbe9336aefa3c
naviret <111366578+naviret@users.noreply.github.com>
2023-11-12 09:36:10 -0500

    added scroll animations on index.html

---
+++ scripts/animations.js
@@ -0,0 +1,31 @@
+let animation = {
+    revealDistance: '150',
+    initialOpacity: '0',
+    transitionDelay: '0',
+    transitionDuration: '2s',
+    transitionProperty: 'all',
+    transitionTimingFunction: 'ease',
+};
+
+revealableContainers = document.querySelectorAll('.revealable');
+
+function reveal() {
+    for (let i = 0; i < revealableContainers.length; i++) {
+        let windowHeight = window.innerHeight;
+        let topOfRevealableContainer =
+            revealableContainers[i].getBoundingClientRect().top;
+        console.log(windowHeight);
+        if (
+            topOfRevealableContainer <
+            windowHeight - animation.revealDistance
+        ) {
+            /* add the active class to the revealableContainer's classlist */
+            revealableContainers[i].classList.add('active');
+        } else {
+            /* remove the active class to the revealableContainer's classlist */
+            revealableContainers[i].classList.remove('active');
+        }
+    }
+}
+
+window.addEventListener('scroll', reveal);
